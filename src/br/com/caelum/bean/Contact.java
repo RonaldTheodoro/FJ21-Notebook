f6c5427b03f76cd3e37511eb7d4ec435de1f342d
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-04-02 21:31:20 -0300

    add Contact bean

---
+++ src/br/com/caelum/bean/Contact.java
@@ -0,0 +1,51 @@
+package br.com.caelum.bean;
+
+import java.util.Calendar;
+
+public class Contact {
+    private String name;
+    private String address;
+    private String email;
+    private String dateText;
+    private Calendar birthDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDateText() {
+        return dateText;
+    }
+
+    public void setDateText(String dateText) {
+        this.dateText = dateText;
+    }
+
+    public Calendar getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Calendar birthDate) {
+        this.birthDate = birthDate;
+    }
+}
